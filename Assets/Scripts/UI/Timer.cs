6276dabfd18de1043fde04f41ba07b8bfe556a32
Ryan <yewryan@gmail.com>
2026-01-31 03:18:48 +0800

    refactor: rename GameTimer to Timer and add slider support

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -1,18 +1,26 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
-    public class GameTimer : MonoBehaviour
+    public class Timer : MonoBehaviour
     {
         [SerializeField] private float totalTime;
         [SerializeField] private TMP_Text timerText;
+        [SerializeField] private Slider timerSlider;
 
         public event System.Action OnTimerExpired;
 
         private float _timeRemaining;
         private bool _isActive;
-    
+
+        private void Start()
+        {
+            StartTimer();
+        }
+
         public void StartTimer()
         {
             _timeRemaining = totalTime;
@@ -42,10 +50,20 @@ namespace UI
                 _isActive = false;
                 OnTimerExpired?.Invoke();
             }
-        
-            int minutes = Mathf.FloorToInt(_timeRemaining / 60f);
-            int seconds = Mathf.FloorToInt(_timeRemaining % 60f);
-            timerText.text = $"{minutes:00}:{seconds:00}";
+
+
+            if (timerText != null)
+            {
+                int minutes = Mathf.FloorToInt(_timeRemaining / 60f);
+                int seconds = Mathf.FloorToInt(_timeRemaining % 60f);
+                timerText.text = $"{minutes:00}:{seconds:00}";
+            }
+
+            if (timerSlider != null)
+            {
+                timerSlider.value = _timeRemaining / totalTime;
+            }
+
         }
     }
 }
